cc0895b4b75a2cdcb35245c66dc73512975ed88e
Tomonori Tanabe <tanb+github@me.com>
2014-11-11 02:32:49 +0900

    Add simpholders2 cask

---
+++ Casks/simpholders2.rb
@@ -0,0 +1,10 @@
+class Simpholders2 < Cask
+  homepage 'http://simpholders.com'
+  url 'http://simpholders.com/site/assets/files/1098/sp20a-87.zip'
+  version '2.0a'
+  sha256 '53c62d2a5817e0b7c3bb77b83dca74819ecee8e799ddc5a0832787a2456ed0a4'
+
+  depends_on :macos => :yosemite # Needs at least Mac OS X "Yosemite" aka. 10.10.
+
+  app 'SimPholders2.app'
+end
