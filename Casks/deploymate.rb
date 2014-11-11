ad5728389bd7af22ca45a71c38106ae76b3c0d39
Tomonori Tanabe <tanb+github@me.com>
2014-11-12 01:54:50 +0900

    Add deploymate cask

---
+++ Casks/deploymate.rb
@@ -0,0 +1,10 @@
+class Deploymate < Cask
+  homepage 'http://www.deploymateapp.com'
+  url 'https://s3.amazonaws.com/deploymate/download/Deploymate127.zip'
+  version '1.2.7'
+  sha256 'f0f8aed76ab3dacb38ca1aead71708a3fa5c112e22115368e999a68d9efbb29c'
+
+  depends_on :macos => :lion # Requires Mac OS X 10.7 or later
+
+  app 'Deploymate.app'
+end
