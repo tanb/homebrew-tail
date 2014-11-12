58ff82235218cf270ccd036626c75aeb610cd6fd
Tomonori Tanabe <tanb+github@me.com>
2014-11-13 01:21:18 +0900

    Fixed setup.py option

--- cider.rb
+++ cider.rb
@@ -14,7 +14,7 @@ class Cider < Formula
   def install
     # https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Homebrew-and-Python.md#if-the-software-provides-a-setuppy
     Language::Python.each_python(build) do |python, version|
-      system python, "setup.py", "install", "--prefix=#{prefix}", "--record=installed.txt"
+      system python, "setup.py", "install", "--prefix=#{prefix}", "--single-version-externally-managed", "--record=installed.txt"
     end
   end
 end
