d28446c2f353200bdf5ac0d609c18cf78aedb5c2
Tomonori Tanabe <tanb+github@me.com>
2014-11-20 06:35:28 +0900

    Update hashlook cask version

--- Casks/hashlook.rb
+++ Casks/hashlook.rb
@@ -1,7 +1,7 @@
 class Hashlook < Cask
   homepage 'https://github.com/tanb/HashLook'
-  url 'https://github.com/tanb/HashLook-Preview/releases/download/0.1.0/HashLook-0.1.0.zip'
-  version '0.1.0'
+  url 'https://github.com/tanb/HashLook-Preview/releases/download/1.0.0/HashLook-1.0.0.zip'
+  version '1.0.0'
   sha256 '752f36862b441cda16fa7d438f1773c377940738b5e9b916da7eb3d7c915f040'
 
   depends_on :macos => :mavericks
