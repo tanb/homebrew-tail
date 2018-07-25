616cd51972fd7feff1879e2f6893d3b60ca3c651
tanb <tanb@me.com>
2018-07-25 14:01:01 +0900

    Fixed

--- pipenv-pipes.rb
+++ pipenv-pipes.rb
@@ -20,6 +20,11 @@ class PipenvPipes < Formula
     sha256 "48eb22f4f8461b1df5734a074b57042430fb06e1d61bd1e11b078c0fe6d7a1f1"
   end
 
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/95/d9/c3336b6b5711c3ab9d1d3a80f1a3e2afeb9d8c02a7166462f6cc96570897/click-6.7.tar.gz"
+    sha256 "f15516df478d5a56180fbf80e68f206010e6d160fc39fa508b65e035fd75130b"
+  end
+
   def install
     # Using the virtualenv DSL here because the alternative of using
     # write_env_script to set a PYTHONPATH breaks things.
@@ -28,10 +33,10 @@ class PipenvPipes < Formula
     venv.pip_install resources
     venv.pip_install buildpath
 
-    # `pipes` needs to be able to find `colorama` and `pewtwo` on PATH. So we
+    # `pipes` needs to be able to find `colorama` and `click` on PATH. So we
     # install symlinks for those scripts in `#{libexec}/tools` and create a
     # wrapper script for `pipenv` which adds `#{libexec}/tools` to PATH.
-    (libexec/"tools").install_symlink libexec/"bin/colorama", libexec/"bin/pipes"
+    (libexec/"tools").install_symlink libexec/"bin/colorama", libexec/"bin/pipes", libexec/"bin/click"
 
     env = {
       :PATH => "#{libexec}/tools:$PATH",
